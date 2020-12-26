7ed9801f279f9d83963b886c6db5d065bc609d76
Bobby Neal <rsneal01@gmail.com>
2020-12-26 18:59:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    params[:number].to_i.times do
+    # params[:number].to_i.times do
       "#{params[:phrase]}"
     end
   end
