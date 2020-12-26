9d60efe09a691aab6ec4d83895656d3ef4e2bccd
Bobby Neal <rsneal01@gmail.com>
2020-12-26 19:39:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,8 +27,8 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
   
-  # get '/:operation/:number1/:number2' do
-  #   "#{params[:number1].to_i params[:operation] params[:number2]}"
-  # end
+  get '/:operation/:number1/:number2' do
+    "#{params[:number1].to_i params[:operation] params[:number2]}"
+  end
   
 end
